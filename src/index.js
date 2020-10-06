81ea3664a651db21e4cf40c0fca464d4d94e3994
Aleksandr Zanko <58461608+AleksandrZanko@users.noreply.github.com>
2020-10-06 12:49:19 +0300

    feat: add my decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+	let str = n.toString();
+	let arr = [];
+	
+	if (str[0] === '-') {
+		for(let i = str.length; i > 0; i--) {
+			arr.push(str[i]);		
+		}
+		let minusStr = arr.join('');
+		return Number(minusStr);
+	} else {
+			return reverseStr = str.split('').reverse().join('');
+			let result = Number(reverseStr);
+			return result;
+	}
 }
